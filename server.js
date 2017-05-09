7fe698dbd958bbbc5a2dc07751e2d99fbc942297
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-09 19:01:35 -0400

    Create a route for testing User schema

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var User = require('./models/user');
 
 var app = express();
 
@@ -13,11 +16,27 @@ mongoose.connect('mongodb://root:cheers21@ds133388.mlab.com:33388/buy_me_a_feeli
 });
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
   var name = "Ken";
   res.json("My name is " + name);
 });
+
+app.post('/create-user', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save(function(err) {
+    if (err) next(err);
+
+    res.json("Successfully created a new user");
+  });
+})
 
 app.listen(3000, function(err) {
   if (err) throw err;
