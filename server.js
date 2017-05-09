baee1143d5d05a292e22498a8ba16712aecd8509
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-09 18:41:31 -0400

    Set up database connection

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
+
+mongoose.connect('mongodb://root:cheers21@ds133388.mlab.com:33388/buy_me_a_feeling', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to the database");
+  }
+});
 
 app.use(morgan('dev'));
 
