fceedcf00313b19e6a101ce9667013d1baa98721
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-13 17:30:38 -0400

    Create get route for generating fake product data

---
+++ api/api.js
@@ -0,0 +1,34 @@
+var router = require('express').Router;
+var async = require('async');
+var faker = require('faker');
+var Category = require('../model/category');
+var Product = require('../model/product');
+
+router.get('/:name', function(req, res, next) {
+
+  async.waterfall([
+
+    function(callback) {
+      Category.findOne({ name: req.params.name }, function(err, category) {
+        if (err) retrn next(err);
+        callback(null, category);
+      });
+    },
+
+    function(category, callback) {
+      for (var i = 0; i < 30; i++) {
+        var product = new Product();
+        product.category = category._id;
+        product.name = faker.commerce.productName();
+        product.price = faker.commerce.price();
+        product.image = faker.image.image();
+
+        product.save();
+      }
+    }
+
+  ]);
+
+  res.json({ message: 'Success' })
+
+});
