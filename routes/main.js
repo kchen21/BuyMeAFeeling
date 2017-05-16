b59a37a8adcac59c2eb1a406c1c525186b3dd67f
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-15 23:22:24 -0400

    Update routes and products page to make pagination possible

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,27 @@
 var router = require('express').Router();
 var Product = require('../models/product');
 
+function paginate(req, res, next) {
+  var perPage = 9;
+  var page = req.params.page || 1;
+
+  Product
+    .find()
+    .skip(perPage * (page - 1))
+    .limit( perPage )
+    .populate('category')
+    .exec(function(err, products) {
+      if (err) return next(err);
+      Product.count().exec(function(err, count) {
+        if (err) return next(err);
+        res.render('main/products-main', {
+          products: products,
+          pages: Math.ceil(count / perPage)
+        });
+      });
+    });
+}
+
 Product.createMapping(function(err, mapping) {
   if (err) {
     console.log("error creating mapping");
@@ -51,28 +72,15 @@ router.get('/search', function(req, res, next) {
 router.get('/', function(req, res, next) {
 
   if (req.user) {
-    var perPage = 9;
-    var page = req.params.page;
-
-    Product
-      .find()
-      .skip(perPage * (page - 1))
-      .limit( perPage )
-      .populate('category')
-      .exec(function(err, products) {
-        if (err) return next(err);
-        Product.count().exec(function(err, count) {
-          if (err) return next(err);
-          res.render('main/product-main', {
-            products: products,
-            pages: count / perPage
-          });
-        });
-      });
+    paginate(req, res, next);
   } else {
     res.render('main/home');
   }
 
+});
+
+router.get('/page/:page', function(req, res, next) {
+  paginate(req, res, next);
 });
 
 router.get('/about', function(req, res) {
