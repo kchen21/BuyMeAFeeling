3b680e5f023b6d7234a8ef08100b6f00a70def21
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-18 19:35:37 -0400

    Create functions to update quantity and total cost of product to add to cart

--- public/js/custom.js
+++ public/js/custom.js
@@ -41,3 +41,34 @@ $(function() {
     });
   });
 });
+
+$(document).on('click', '#plus', function(e) {
+  e.preventDefault();
+  var priceValue = parseFloat($('#priceValue').val());
+  var quantity = parseInt($('#quantity').val());
+
+  priceValue += parseFloat($('#priceOfOne').val());
+  quantity += 1;
+
+  $('#quantity').val(quantity);
+  $('#priceValue').val(priceValue.toFixed(2));
+  $('#total').html(quantity);
+});
+
+$(document).on('click', '#minus', function(e) {
+  e.preventDefault();
+  var priceValue = parseFloat($('#priceValue').val());
+  var quantity = parseInt($('#quantity').val());
+
+  if (quantity === 1) {
+    priceValue = $('#priceOfOne').val();
+    quantity = 1;
+  } else {  
+    priceValue -= parseFloat($('#priceOfOne').val());
+    quantity -= 1;
+  }
+
+  $('#quantity').val(quantity);
+  $('#priceValue').val(priceValue.toFixed(2));
+  $('#total').html(quantity);
+});
