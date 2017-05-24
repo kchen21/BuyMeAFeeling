6b3f9b5da1f5aa3ff721571f315cd7a83b506d50
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-24 11:35:58 -0400

    Update port

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports = {
   database: 'mongodb://heroku_534rjp6p:8pp16dftom8mnjo5kid7je9u90@ds151431.mlab.com:51431/heroku_534rjp6p',
-  port: 3000,
+  port: process.env.PORT || 3000,
   secretKey: "Kensington$@!",
 
   facebook: {
