00ab54f53136d5c2d3c6de66bdeb2447fe7e5771
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-24 15:57:56 -0400

    Update order of product get routes

--- api/api.js
+++ api/api.js
@@ -15,35 +15,6 @@ router.post('/search', function(req, res, next) {
   });
 });
 
-router.get('/:name', function(req, res, next) {
-
-  async.waterfall([
-
-    function(callback) {
-      Category.findOne({ name: req.params.name }, function(err, category) {
-        if (err) return next(err);
-        callback(null, category);
-      });
-    },
-
-    function(category, callback) {
-      for (var i = 0; i < 30; i++) {
-        var product = new Product();
-        product.category = category._id;
-        product.name = faker.commerce.productName();
-        product.price = faker.commerce.price();
-        product.image = faker.image.image();
-
-        product.save();
-      }
-    }
-
-  ]);
-
-  res.json({ message: 'Success' });
-
-});
-
 router.get('/positive', function(req, res, next) {
 
   async.waterfall([
@@ -120,6 +91,35 @@ router.get('/miscellaneous', function(req, res, next) {
         product.name = feeling.name;
         product.price = feeling.price;
         product.image = feeling.image;
+
+        product.save();
+      }
+    }
+
+  ]);
+
+  res.json({ message: 'Success' });
+
+});
+
+router.get('/:name', function(req, res, next) {
+
+  async.waterfall([
+
+    function(callback) {
+      Category.findOne({ name: req.params.name }, function(err, category) {
+        if (err) return next(err);
+        callback(null, category);
+      });
+    },
+
+    function(category, callback) {
+      for (var i = 0; i < 30; i++) {
+        var product = new Product();
+        product.category = category._id;
+        product.name = faker.commerce.productName();
+        product.price = faker.commerce.price();
+        product.image = faker.image.image();
 
         product.save();
       }
