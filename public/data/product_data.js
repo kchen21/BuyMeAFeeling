f4120aa64a7df5716b638621ec0d829f166a7582
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-24 16:49:10 -0400

    Update product image paths in product data

--- public/data/product_data.js
+++ public/data/product_data.js
@@ -4,82 +4,82 @@ productData = {
     affectionate: {
       name: 'affectionate',
       price: 5,
-      image: '/public/img/emojis/affectionate.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/affectionate.png'
     },
     angelic: {
       name: 'angelic',
       price: 2,
-      image: '/public/img/emojis/angelic.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/angelic.png'
     },
     confident: {
       name: 'confident',
       price: 3,
-      image: '/public/img/emojis/confident.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/confident.png'
     },
     cool: {
       name: 'cool',
       price: 6,
-      image: '/public/img/emojis/cool.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/cool.png'
     },
     devilish: {
       name: 'devilish',
       price: 8,
-      image: '/public/img/emojis/devilish.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/devilish.png'
     },
     engaged: {
       name: 'engaged',
       price: 1,
-      image: '/public/img/emojis/engaged.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/engaged.png'
     },
     excited: {
       name: 'excited',
       price: 2,
-      image: '/public/img/emojis/excited.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/excited.png'
     },
     flirtatious: {
-      name: 'fliratatious',
+      name: 'flirtatious',
       price: 5,
-      image: '/public/img/emojis/fliratatious.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/fliratatious.png'
     },
     happy: {
       name: 'happy',
       price: 7,
-      image: '/public/img/emojis/happy.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/happy.png'
     },
     nerdy: {
       name: 'nerdy',
       price: 9,
-      image: '/public/img/emojis/nerdy.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/nerdy.png'
     },
     relieved: {
       name: 'relieved',
       price: 2,
-      image: '/public/img/emojis/relieved.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/relieved.png'
     },
     satiated: {
       name: 'satiated',
       price: 1,
-      image: '/public/img/emojis/satiated.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/satiated.png'
     },
     silly: {
       name: 'silly',
       price: 8,
-      image: '/public/img/emojis/silly.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/silly.png'
     },
     smug: {
       name: 'smug',
       price: 5,
-      image: '/public/img/emojis/smug.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/smug.png'
     },
     warm: {
       name: 'warm',
       price: 3,
-      image: '/public/img/emojis/warm.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/warm.png'
     },
     worried: {
       name: 'worried',
       price: 1,
-      image: '/public/img/emojis/worried.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/worried.png'
     }
   },
 
@@ -87,72 +87,72 @@ productData = {
     angry: {
       name: 'angry',
       price: 2,
-      image: '/public/img/emojis/angry.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/angry.png'
     },
     bored: {
       name: 'bored',
       price: 6,
-      image: '/public/img/emojis/bored.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/bored.png'
     },
     confused: {
       name: 'confused',
       price: 3,
-      image: '/public/img/emojis/confused.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/confused.png'
     },
     despairing: {
       name: 'despairing',
       price: 5,
-      image: '/public/img/emojis/despairing.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/despairing.png'
     },
     disappointed: {
       name: 'disappointed',
       price: 7,
-      image: '/public/img/emojis/disappointed.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/disappointed.png'
     },
     dizzy: {
       name: 'dizzy',
       price: 4,
-      image: '/public/img/emojis/dizzy.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/dizzy.png'
     },
     exasperated: {
       name: 'exasperated',
       price: 2,
-      image: '/public/img/emojis/exasperated.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/exasperated.png'
     },
     horrified: {
       name: 'horrified',
       price: 1,
-      image: '/public/img/emojis/horrified.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/horrified.png'
     },
     hushed: {
       name: 'hushed',
       price: 5,
-      image: '/public/img/emojis/hushed.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/hushed.png'
     },
     nervous: {
       name: 'nervous',
       price: 8,
-      image: '/public/img/emojis/nervous.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/nervous.png'
     },
     remorseful: {
       name: 'remorseful',
       price: 9,
-      image: '/public/img/emojis/remorseful.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/remorseful.png'
     },
     sad: {
       name: 'sad',
       price: 7,
-      image: '/public/img/emojis/sad.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/sad.png'
     },
     sick: {
       name: 'sick',
       price: 4,
-      image: '/public/img/emojis/sick.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/sick.png'
     },
     unamused: {
       name: 'unamused',
       price: 3,
-      image: '/public/img/emojis/unamused.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/unamused.png'
     }
   },
 
@@ -160,22 +160,22 @@ productData = {
     neutral: {
       name: 'neutral',
       price: 5,
-      image: '/public/img/emojis/neutral.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/neutral.png'
     },
     speechless: {
       name: 'speechless',
       price: 8,
-      image: '/public/img/emojis/speechless.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/speechless.png'
     },
     starstruck: {
       name: 'starstruck',
       price: 1,
-      image: '/public/img/emojis/starstruck.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/starstruck.png'
     },
     surprised: {
       name: 'surprised',
       price: 9,
-      image: '/public/img/emojis/surprised.png'
+      image: 'https://raw.githubusercontent.com/kchen21/BuyMeAFeeling/master/public/img/emojis/surprised.png'
     }
   }
 
