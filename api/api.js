dba0e7cd51dfa5265ccf7e55517aa3bc95d52621
Kenneth Chen <kennethchendbgt@gmail.com>
2017-05-24 15:54:07 -0400

    Create routes for getting products

--- api/api.js
+++ api/api.js
@@ -4,6 +4,8 @@ var faker = require('faker');
 var Category = require('../models/category');
 var Product = require('../models/product');
 
+var productData = require('../public/data/product_data');
+
 router.post('/search', function(req, res, next) {
   Product.search({
     query_string: { query: req.body.search_string },
@@ -38,7 +40,94 @@ router.get('/:name', function(req, res, next) {
 
   ]);
 
-  res.json({ message: 'Success' })
+  res.json({ message: 'Success' });
+
+});
+
+router.get('/positive', function(req, res, next) {
+
+  async.waterfall([
+
+    function(callback) {
+      Category.findOne({ name: 'positive' }, function(err, positiveCategory) {
+        if (err) return next(err);
+        callback(null, positiveCategory);
+      });
+    },
+
+    function(positiveCategory, callback) {
+      for (var feeling in productData.positive) {
+        var product = new Product();
+        product.category = positiveCategory._id;
+        product.name = feeling.name;
+        product.price = feeling.price;
+        product.image = feeling.image;
+
+        product.save();
+      }
+    }
+
+  ]);
+
+  res.json({ message: 'Success' });
+
+});
+
+router.get('/negative', function(req, res, next) {
+
+  async.waterfall([
+
+    function(callback) {
+      Category.findOne({ name: 'negative' }, function(err, negativeCategory) {
+        if (err) return next(err);
+        callback(null, negativeCategory);
+      });
+    },
+
+    function(negativeCategory, callback) {
+      for (var feeling in productData.negative) {
+        var product = new Product();
+        product.category = negativeCategory._id;
+        product.name = feeling.name;
+        product.price = feeling.price;
+        product.image = feeling.image;
+
+        product.save();
+      }
+    }
+
+  ]);
+
+  res.json({ message: 'Success' });
+
+});
+
+router.get('/miscellaneous', function(req, res, next) {
+
+  async.waterfall([
+
+    function(callback) {
+      Category.findOne({ name: 'miscellaneous' }, function(err, miscellaneousCategory) {
+        if (err) return next(err);
+        callback(null, miscellaneousCategory);
+      });
+    },
+
+    function(miscellaneousCategory, callback) {
+      for (var feeling in productData.miscellaneous) {
+        var product = new Product();
+        product.category = miscellaneousCategory._id;
+        product.name = feeling.name;
+        product.price = feeling.price;
+        product.image = feeling.image;
+
+        product.save();
+      }
+    }
+
+  ]);
+
+  res.json({ message: 'Success' });
 
 });
 
